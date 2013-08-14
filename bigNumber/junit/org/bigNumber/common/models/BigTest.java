2658595b8283ca6f7fd99d88eab3a4cd557895c4
shuklaalok7 <shuklaalok7@gmail.com>
2013-08-14 14:56:12 +0530

    Added junit path and library

---
+++ bigNumber/junit/org/bigNumber/common/models/BigTest.java
@@ -0,0 +1,28 @@
+package org.bigNumber.common.models;
+
+import junit.framework.TestCase;
+
+public class BigTest extends TestCase {
+	
+	/*private Big no1 = null;
+	private Big no2 = null;
+	
+	protected Big getNo1() {
+		if(no1 == null)
+			return new Big();
+		return no1;
+	}
+	
+	protected Big getNo2() {
+		if(no2 == null)
+			return new Big();
+		return no2;
+	}
+	
+	protected Big setNo1() {
+		if(no1 == null)
+			return new Big();
+		return no1;
+	}*/
+	
+}
