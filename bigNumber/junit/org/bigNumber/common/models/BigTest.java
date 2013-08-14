48e83eeb3aa7b5a818ba9e03c4d830f6c6ab2d60
shuklaalok7 <shuklaalok7@gmail.com>
2013-08-14 19:31:48 +0530

    TestCase

--- bigNumber/junit/org/bigNumber/common/models/BigTest.java
+++ bigNumber/junit/org/bigNumber/common/models/BigTest.java
@@ -1,5 +1,10 @@
 package org.bigNumber.common.models;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.bigNumber.common.services.exceptions.IncompatibleCharacterException;
+import org.junit.Test;
 import junit.framework.TestCase;
 
 public class BigTest extends TestCase {
@@ -25,6 +30,22 @@ public class BigTest extends TestCase {
 		return no1;
 	}*/
 	
-	
+	@Test
+	public void roundOffTest() {
+		Big no = new Big();
+		try {
+			no.setValue("34.2896");
+		} catch (IncompatibleCharacterException e) {
+			e.printStackTrace();
+		}
+		List<Character> result        = no.getValue();
+		List<Character> correctResult = new ArrayList<Character>();
+		correctResult.add('3');
+		correctResult.add('4');
+		correctResult.add('.');
+		correctResult.add('2');
+		correctResult.add('9');
+		assertTrue(result == correctResult);
+	}
 	
 }
